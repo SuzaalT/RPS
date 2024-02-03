f2774229254bda4d26e9f8744e742e4522cf266a
Sujal Thapa <suzaalproff@gmail.com>
2024-02-02 21:16:48 -0500

    Added another function to reduce the code amount

--- script.js
+++ script.js
@@ -1,3 +1,58 @@
+function playgame(playermove){
+  const computerMove =pickComputerMove();
+  let result='';
+
+  if(playermove === 'rock'){
+    if(computerMove === 'paper'){
+      result = 'You lose'
+    }else if ( computerMove  === 'rock'){
+      result = 'Its a Tie'
+    }else if ( computerMove === 'scissors'){
+      result = ' You lost'
+    }
+  }else if (playermove === 'scissors'){
+    if(computerMove === 'rock '){
+      result = 'you lose'
+    }else if ( playermove === 'scissors'){
+      result = 'Its a Tie'
+    }else if ( playermove === 'paper'){
+      result = ' You Win'
+    }
+  }else if (playermove === 'paper'){
+    if (computerMove === 'scissors'){
+      result = 'You lose'
+    } else if ( computerMove === 'paper'){
+      result = 'Its a Tie'
+    } else if ( computerMove === 'rock'){
+      result = ' You Win'
+    }
+    alert (` You picked ${playermove}. Computer picked ${computerMove}.${result}`);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function pickComputerMove() {
   let computerMove = "";
 
