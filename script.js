8757ef9e844d2f367303a6e0ba3ca18a457666c3
Sujal Thapa <suzaalproff@gmail.com>
2024-02-02 20:32:11 -0500

    First Function Successful

---
+++ script.js
@@ -0,0 +1,14 @@
+function pickComputerMove() {
+  let computerMove = "";
+
+  const randomNumber = Math.random();
+
+  if (randomNumber >= 0 && randomNumber < 1 / 3) {
+    computerMove = "rock";
+  } else if (randomNumber >= 1 / 3 && randomNumber < 2 / 3) {
+    computerMove = "paper";
+  } else if (randomNumber >= 2 / 3 && randomNumber < 1) {
+    computerMove = "scissors";
+  }
+  return computerMove;
+}
